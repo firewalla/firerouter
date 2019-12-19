3ccfa5e22717131c551908fa46dc492a20297cd1
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-12-19 19:06:11 +0800

    add isolated option in interface config

--- core/network_setup.js
+++ core/network_setup.js
@@ -54,11 +54,13 @@ class NetworkSetup {
     // create routing tables
     await routing.createCustomizedRoutingTable(routing.RT_GLOBAL_LOCAL);
     await routing.createCustomizedRoutingTable(routing.RT_GLOBAL_DEFAULT);
-    await routing.createCustomizedRoutingTable(routing.RT_ROUTABLE);
+    await routing.createCustomizedRoutingTable(routing.RT_WAN_ROUTABLE);
+    await routing.createCustomizedRoutingTable(routing.RT_LAN_ROUTABLE);
     await routing.createCustomizedRoutingTable(routing.RT_STATIC);
     await routing.flushRoutingTable(routing.RT_GLOBAL_LOCAL);
     await routing.flushRoutingTable(routing.RT_GLOBAL_DEFAULT);
-    await routing.flushRoutingTable(routing.RT_ROUTABLE);
+    await routing.flushRoutingTable(routing.RT_WAN_ROUTABLE);
+    await routing.flushRoutingTable(routing.RT_LAN_ROUTABLE);
     await routing.flushRoutingTable(routing.RT_STATIC);
 
     await routing.createPolicyRoutingRule("all", null, routing.RT_GLOBAL_LOCAL, 3000);
