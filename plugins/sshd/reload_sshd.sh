c36b0c50b32e027bf9f7b55387a7333982c516f2
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-12-26 16:44:50 +0800

    support sshd plugin

---
+++ plugins/sshd/reload_sshd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+LISTEN_ADDRESSES=""
+
+for CONF_FILE in /home/pi/.router/config/sshd/sshd_config.*; do
+  LISTEN_ADDRESSES="$LISTEN_ADDRESSES$(cat $CONF_FILE)\n"
+done
+
+sed "s/#LISTEN_ADDRESSES#/$LISTEN_ADDRESSES/g" $CUR_DIR/default_sshd_config > /home/pi/.router/config/sshd/sshd_config
+
+sudo cp /home/pi/.router/config/sshd/sshd_config /etc/ssh/sshd_config
+
+sudo systemctl reload sshd
+
