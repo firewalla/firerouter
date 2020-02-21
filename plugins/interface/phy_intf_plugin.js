0656833031136950a625554d58c45baeed478794
Edison Liu <jasonlyc.sjtu@gmail.com>
2020-02-21 17:21:56 +0800

    use dhcpcd to handle ipv6 router advertisement and stateful DHCPv6

--- plugins/interface/phy_intf_plugin.js
+++ plugins/interface/phy_intf_plugin.js
@@ -30,8 +30,13 @@ class PhyInterfacePlugin extends InterfaceBasePlugin {
     // copy dhclient hook script
     await exec(`sudo rm -f /etc/dhcp/dhclient-exit-hooks.d/firerouter_*`).catch((err) => {});
     await exec(`sudo cp ${r.getFireRouterHome()}/scripts/firerouter_dhclient_ip_change /etc/dhcp/dhclient-exit-hooks.d/`);
-    // copy firerouter_dhclient.service
+    // copy dhcpcd hook script
+    await exec(`sudo rm -r /lib/dhcpcd/dhcpcd-hooks/firerouter_*`).catch((err) => {});
+    await exec(`sudo cp ${r.getFireRouterHome()}/scripts/firerouter_dhcpcd_update_rt /lib/dhcpcd/dhcpcd-hooks/`);
+    // copy firerouter_dhclient@.service
     await exec(`sudo cp ${r.getFireRouterHome()}/scripts/firerouter_dhclient@.service /etc/systemd/system/`);
+    // copy firerouter_dhcpcd6@.service
+    await exec(`sudo cp ${r.getFireRouterHome()}/scripts/firerouter_dhcpcd6@.service /etc/systemd/system/`);
     await exec("sudo systemctl daemon-reload");
   }
 
