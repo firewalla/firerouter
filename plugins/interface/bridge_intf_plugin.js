9a6382bca5baff76bfe0045ff290ad14f541589e
Edison Liu <jasonlyc.sjtu@gmail.com>
2020-05-26 12:41:33 +0800

    do not enable stp on bridge if there is only one sub interface

--- plugins/interface/bridge_intf_plugin.js
+++ plugins/interface/bridge_intf_plugin.js
@@ -36,9 +36,12 @@ class BridgeInterfacePlugin extends InterfaceBasePlugin {
     await exec(`sudo brctl addbr ${this.name}`).catch((err) => {
       this.log.error(`Failed to create bridge interface ${this.name}`, err.message);
     });
-    await exec(`sudo brctl stp ${this.name} on`).catch((err) => {
-      this.log.error(`Failed to enable stp on bridge interface ${this.name}`, err.message);
-    })
+    // no need to enable stp if there is only one interface in bridge
+    if (this.networkConfig.intf.length > 1) {
+      await exec(`sudo brctl stp ${this.name} on`).catch((err) => {
+        this.log.error(`Failed to enable stp on bridge interface ${this.name}`, err.message);
+      });
+    }
     await exec(`sudo brctl addif ${this.name} ${this.networkConfig.intf.join(" ")}`).catch((err) => {
       this.log.error(`Failed to add interfaces to bridge ${this.name}`, err.message);
     });
