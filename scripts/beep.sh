16d0aaef25649307df986520d0cd3f4ffdb01082
Jian Yang <pub@yangjian.net>
2020-09-24 16:47:19 +0800

    only beep once if bluetooth found in dmesg

--- scripts/beep.sh
+++ scripts/beep.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-NUM=1
+# execute Firewalla beep.sh if any
+FW_BEEP=/home/pi/firewalla/scripts/beep.sh
+test -x $FW_BEEP ]] && exec $FW_BEEP $@
 
-if [[ -n $1 ]]; then
-  NUM=$1
-fi
+NUM=${1:-'1'}
 
 test $NUM -gt 10 && exit 0
+
+if [[ $NUM -eq 1 ]]; then
+    dmesg | tail -n 10 | fgrep -q CSR8510 || exit 1
+fi
 
 test $(redis-cli type sys:nobeep) != "none" && redis-cli del sys:nobeep && exit 0
 
