f633ec967e4cda8eb398150d7be0218fb63dd64d
Jian Yang <pub@yangjian.net>
2020-10-21 18:17:57 +0800

    fix multiple lines of matching

--- scripts/beep.sh
+++ scripts/beep.sh
@@ -10,7 +10,7 @@ test $NUM -gt 10 && exit 0
 
 if [[ $NUM -eq 1 ]]; then
     time_now=$(date +%s)
-    time_bt=$(date -d "$(dmesg -T | sed -n '/ CSR8510 / s/\[\(.*\)\].*/\1/p')" +%s)
+    time_bt=$(date -d "$(dmesg -T | sed -n '/ CSR8510 / s/\[\(.*\)\].*/\1/p'|tail -1)" +%s)
     let time_diff=time_now-time_bt
     # beep once only when bluetooth inserted within last 30 seconds
     test $time_diff -gt 30 && exit 1
