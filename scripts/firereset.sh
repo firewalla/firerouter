d59abcd7ffbf96f6ee0147f4e779b1cd190ad84a
Edison Liu <jasonlyc.sjtu@gmail.com>
2021-03-29 19:09:08 +0800

    clean log partition in firereset.sh

--- scripts/firereset.sh
+++ scripts/firereset.sh
@@ -6,6 +6,8 @@ LICENSE_FILE=/home/pi/.firewalla/license
 source /home/pi/firewalla/platform/platform.sh
 source $DIR/../platform/platform.sh
 
+bash /home/pi/firewalla/scripts/clean_log.sh || true
+
 run_host_light_until_paired() {
   type run_horse_light || return 1
   while [[ ! -e $LICENSE_FILE ]]; do
