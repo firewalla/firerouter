105bcab56701f8c57a96bc4808081cee8ad2dc43
LAPTOP-6POHS2HL\cli8910 <cli880910@gmail.com>
2021-07-08 12:30:47 +0800

    record service restart metrics

--- plugins/dhcp/dhcp.template.sh
+++ plugins/dhcp/dhcp.template.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source %FIREROUTER_HOME%/platform/platform.sh
 DNSMASQ_BINARY=$(get_dnsmasq_path)
-
-$DNSMASQ_BINARY -k --clear-on-reload -u pi -C %FIREROUTER_HOME%/etc/dnsmasq.dhcp.default.conf & redis-cli HINCRBY "stats:systemd:restart" firerouter_dhcp 1 &
+redis-cli HINCRBY "stats:systemd:restart" firerouter_dhcp 1
+$DNSMASQ_BINARY -k --clear-on-reload -u pi -C %FIREROUTER_HOME%/etc/dnsmasq.dhcp.default.conf &
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 for job in `jobs -p`; do wait $job; echo "$job exited"; done
