26a63eb5203d769d19f075cead658ade90f0d40e
Melvin Tu <melvinto@gmail.com>
2021-08-19 14:40:04 +0800

    exit gracefully on post scripts

--- scripts/post_start_firereset.sh
+++ scripts/post_start_firereset.sh
@@ -4,3 +4,5 @@
 source ${FIREROUTER_HOME}/platform/platform.sh
 
 ${NEED_FIRESTATUS:=false} && curl 'http://127.0.0.1:9966/resolve?name=firereset&type=critical_error'
+
+exit 0
--- scripts/post_stop_firereset.sh
+++ scripts/post_stop_firereset.sh
@@ -5,3 +5,5 @@ source ${FIREROUTER_HOME}/platform/platform.sh
 
 # firereset should never be stopped
 ${NEED_FIRESTATUS:=false} && curl 'http://127.0.0.1:9966/fire?name=firereset&type=critical_error'
+
+exit 0
