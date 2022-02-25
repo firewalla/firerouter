2444cc45c64b77d8c17a7057b05ff78388a3bee4
Edison Liu <jasonlyc.sjtu@gmail.com>
2022-02-25 14:02:54 +0800

    also add peer networks to main routing table

--- plugins/interface/wireguard_intf_plugin.js
+++ plugins/interface/wireguard_intf_plugin.js
@@ -121,6 +121,7 @@ class WireguardInterfacePlugin extends InterfaceBasePlugin {
         if (peer.allowedIPs) {
           for (const allowedIP of peer.allowedIPs) {
             // route for allowed IP has a lower priority, in case there are conflicts between allowedIPs and other LAN IPs
+            await routing.addRouteToTable(allowedIP, null, this.name, "main", 512, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});
             if (this.isLAN()) {
               // add peer networks to wan_routable and lan_routable
               await routing.addRouteToTable(allowedIP, null, this.name, routing.RT_LAN_ROUTABLE, 512, new Address4(allowedIP).isValid() ? 4 : 6).catch((err) => {});
