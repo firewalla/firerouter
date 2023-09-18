7fdd7ca6f979556b4a02859442cc1645ef36b596
Melvin <108800+MelvinTo@users.noreply.github.com>
2023-09-17 23:51:16 -0500

    Update assets_controller.js
    change uptime to u
    change procuptime to pu

--- core/assets_controller.js
+++ core/assets_controller.js
@@ -131,7 +131,7 @@ class AssetsController {
     }
 
     if (mac) {
-      const assetsStatus = {ts: Date.now() / 1000, mac: mac, sysUptime: msg.uptime, procUptime: msg.process_uptime, version: msg.version, channelUtilization: msg.util, wanMode: msg.wanMode, upstreamAPs: msg.upstreamAPs};
+      const assetsStatus = {ts: Date.now() / 1000, mac: mac, sysUptime: msg.u, procUptime: msg.pu, version: msg.version, channelUtilization: msg.util, wanMode: msg.wanMode, upstreamAPs: msg.upstreamAPs};
       await rclient.hsetAsync(KEY_ASSETS_STATUS, mac, JSON.stringify(assetsStatus));
     }
   }
