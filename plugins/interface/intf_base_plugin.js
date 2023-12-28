921854a7f7b9298fccbd24d270299646a049f7da
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2023-12-28 17:30:13 +0800

    Revert "skip setMTU if current MTU is same as the configured value"
    This reverts commit 2740e1d6f7ed9a70eab691399c9758d34b969ab9.

--- plugins/interface/intf_base_plugin.js
+++ plugins/interface/intf_base_plugin.js
@@ -702,17 +702,9 @@ class InterfaceBasePlugin extends Plugin {
     return null;
   }
 
-  async getMTU() {
-    return fs.readFileAsync(`/sys/class/net/${this.name}/mtu`, {encoding: "utf8"}).then(result => Number(result.trim())).catch((err) => {
-      this.log.error(`Failed to get MTU of ${this.name}`, err.message);
-      return null;
-    });
-  }
-
   async setMTU() {
     const mtu = this.networkConfig.mtu || this.getDefaultMTU();
-    const currentMTU = await this.getMTU();
-    if (mtu && mtu !== currentMTU)
+    if (mtu)
       await platform.setMTU(this.name, mtu);
   }
 
