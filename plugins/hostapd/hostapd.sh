1ff45b73aa07f781daa447ddeadb4c444480bf11
Melvin <melvinto@gmail.com>
2025-09-16 12:22:50 +0800

    Fix hostapd command execution by removing the background flag, ensuring proper startup of the hostapd service.

--- plugins/hostapd/hostapd.sh
+++ plugins/hostapd/hostapd.sh
@@ -6,4 +6,4 @@ source /home/pi/firerouter/platform/platform.sh
 
 HOSTAPD_BINARY=$(get_hostapd_path)
 
-sudo $HOSTAPD_BINARY -B /home/pi/.router/config/hostapd/$INTF.conf
+sudo $HOSTAPD_BINARY /home/pi/.router/config/hostapd/$INTF.conf
