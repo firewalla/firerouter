8a2b8e17e56c341010f60d3bf3301cbc0d633b3d
Melvin <melvinto@gmail.com>
2025-11-04 16:27:09 +0800

    Remove the curl command for applying current config in wpa_action.sh and add a comment indicating the need for a better solution when handling sta connect events.

--- platform/orange/files/wpa_action.sh
+++ platform/orange/files/wpa_action.sh
@@ -11,7 +11,8 @@ case "$2" in
   CONNECTED)
     redis-cli publish "wpa.connected" "$INTF,$WPA_ID"
     logger "orange wpa_action.sh: applying current config to apply latest $INTF sta connect event"
-    curl -XPOST localhost:8837/v1/config/apply_current_config -o /dev/null || echo "Failed to apply current config to apply latest $INTF sta connect event"
+    # need a better solution for orange to apply current config when sta connect event is received
+    #curl -XPOST localhost:8837/v1/config/apply_current_config -o /dev/null || echo "Failed to apply current config to apply latest $INTF sta connect event"
     ;;
   DISCONNECTED)
     redis-cli publish "wpa.disconnected" "$INTF,$WPA_ID"
