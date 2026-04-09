b06a97d2a17a15c8e5352684b3aa73ba13d88687
melo-crypto <melo@firewalla.com>
2026-04-09 15:28:41 +0800

    add ipv6 passthrough specific process to dhcpcd service

--- scripts/start_dhcpcd_service.sh
+++ scripts/start_dhcpcd_service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "$(date) ipv6_passthrough=${ipv6_passthrough}" >> /tmp/test/dhcpcd6.log
 : ${FIREROUTER_HOME:=/home/pi/firerouter}
 
 source ${FIREROUTER_HOME}/platform/platform.sh
@@ -7,4 +7,4 @@ source ${FIREROUTER_HOME}/platform/platform.sh
 INTF=$1
 DHCPCD_BINARY=$(get_dhcpcd_path)
 
-$DHCPCD_BINARY -6 -t 0 -f "/home/pi/.router/config/dhcpcd6/${INTF}.conf" ${INTF} -e rt_tables="main ${INTF}_local ${INTF}_default" -e default_rt_tables="${INTF}_default" --duid-path "/home/pi/.router/run/dhcpcd-${INTF}.duid"
+$DHCPCD_BINARY -6 -t 0 -f "/home/pi/.router/config/dhcpcd6/${INTF}.conf" ${INTF} -e rt_tables="main ${INTF}_local ${INTF}_default" -e default_rt_tables="${INTF}_default" -e ipv6_passthrough="${ipv6_passthrough}" --duid-path "/home/pi/.router/run/dhcpcd-${INTF}.duid"
