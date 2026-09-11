66337e80aea5abcd7ffb1a9cf90a5b2e733b2499
Yijin Hu <yijin@firewalla.com>
2026-09-11 11:31:23 +0800

    allocate release and beta branches for crystal

--- platform/crystal/platform.sh
+++ platform/crystal/platform.sh
@@ -84,3 +84,23 @@ function get_smcrouted_path {
     ;;
   esac
 }
+
+function map_target_branch {
+  case "$1" in
+  "release_6_0")
+    echo "release_15_0"
+    ;;
+  "beta_6_0")
+    echo "beta_24_0"
+    ;;
+  "beta_7_0")
+    echo "beta_25_0"
+    ;;
+  "master")
+    echo "master"
+    ;;
+  *)
+    echo $1
+    ;;
+  esac
+}
